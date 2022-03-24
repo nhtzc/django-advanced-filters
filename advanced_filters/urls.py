e946d9c348bf81bb6605e53abe32a1a55a40fd18
nhtzc <85615833+nhtzc@users.noreply.github.com>
2022-03-24 21:17:06 +0100

    Update urls.py
    Make django 4.0 compatible

--- advanced_filters/urls.py
+++ advanced_filters/urls.py
@@ -1,14 +1,25 @@
-from django.urls import path
-
 from advanced_filters.views import GetFieldChoices
+try:
+    from django.urls import path
+    urlpatterns = [
+        path('field_choices/<model>/<field_name>/',
+            GetFieldChoices.as_view(),
+            name='afilters_get_field_choices'),
 
-urlpatterns = [
-    path('field_choices/<model>/<field_name>/',
-        GetFieldChoices.as_view(),
-        name='afilters_get_field_choices'),
+        # only to allow building dynamically
+        path('field_choices/',
+            GetFieldChoices.as_view(),
+            name='afilters_get_field_choices'),
+    ]
+except ImportError:
+    from django.urls import re_path
+    urlpatterns = [
+        re_path(r'^field_choices/(?P<model>.+)/(?P<field_name>.+)/?',
+                GetFieldChoices.as_view(),
+                name='afilters_get_field_choices'),
 
-    # only to allow building dynamically
-    path('field_choices/',
-        GetFieldChoices.as_view(),
-        name='afilters_get_field_choices'),
-]
+        # only to allow building dynamically
+        re_path(r'^field_choices/$',
+                GetFieldChoices.as_view(),
+                name='afilters_get_field_choices'),
+    ]
